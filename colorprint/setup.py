c5a013233973ab309e3a161ae04e232a6f44b7a5
mmelgar <marcos.melgar.segovia@gmail.com>
2021-05-28 09:33:21 +0200

    initial commit

---
+++ colorprint/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='colorprint',
+version='0.1',
+description='print color for terminal in linux',
+url='#',
+author='@imcosmicwol',
+author_email='',
+license='',
+packages=['colorprint'],
+zip_safe=False)
